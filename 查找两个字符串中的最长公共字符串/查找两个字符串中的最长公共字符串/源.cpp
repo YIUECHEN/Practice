0a9610576ba496a2ec6168735927802fa94e5305
YIUECHEN <2268691528@qq.com>
2019-11-29 20:02:44 +0800

    practice

---
+++ 查找两个字符串中的最长公共字符串/查找两个字符串中的最长公共字符串/源.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	string a, b;
+
+	while (cin >> a >> b){
+		if (a.size() > b.size()){
+			swap(a, b);
+		}
+		string str_m;//存储最长公共子串
+		for (int i = 0; i < a.size(); i++){
+			for (int j = i; j < a.size(); j++){
+				string temp = a.substr(i, j - i + 1);
+				int ret = b.find(temp);
+				if (ret < 0)
+					break;
+				else if (str_m.size() < temp.size()){
+					str_m = temp;
+				}
+			}
+		}
+		
+		cout << str_m << endl;
+	}
+	return 0;
+}
+	
