2b98feb91d8025b62038534899d50d314677751f
YIUECHEN <2268691528@qq.com>
2019-12-01 19:49:07 +0800

    practice

--- 找字符串中第一个只出现一次的字符/找字符串中第一个只出现一次的字符/test.cpp
+++ 找字符串中第一个只出现一次的字符/找字符串中第一个只出现一次的字符/test.cpp
@@ -1,34 +1,59 @@
-#include<string>
+//#include<string>
+//#include<iostream>
+//
+//using namespace std;
+//
+//int firstUniqChar(string s) {
+//		int count[256];
+//		int sz = s.size();
+//
+//		for (int i = 0; i<sz; ++i){
+//
+//			count[s[i]] += 1;
+//		}
+//		for (int i = 0; i<sz; i++){
+//
+//			if (1 == count[s[i]]){
+//
+//				return i;
+//			}
+//		}
+//		return -1;
+//	}
+//
+//int main(){
+//	string s{"hehjilkfid"};
+//		
+//	
+//	char ret=s[firstUniqChar(s)];
+//
+//	cout << ret << endl;
+//
+//		system("pause");
+//		return 0;
+//	}
+
 #include<iostream>
-
+#include<string>
 using namespace std;
-
-int firstUniqChar(string s) {
-		int count[256];
-		int sz = s.size();
-
-		for (int i = 0; i<sz; ++i){
-
-			count[s[i]] += 1;
-		}
-		for (int i = 0; i<sz; i++){
-
-			if (1 == count[s[i]]){
-
-				return i;
-			}
-		}
-		return -1;
-	}
-
 int main(){
-	string s{"hehjilkfid"};
-		
-	
-	char ret=s[firstUniqChar(s)];
+	string str;
+	while (getline(cin, str)){
+		int p = -1;
+		for (int i = 0; i<str.size(); i++){
+			if (str.find(str[i]) == str.rfind(str[i])){
 
-	cout << ret << endl;
+				p = i;
+				break;
+			}
 
-		system("pause");
-		return 0;
+		}
+		if (p == -1){
+			cout << -1 << endl;
+		}
+		else{
+			cout << str[p] << endl;
+		}
 	}
+	return 0;
+}
