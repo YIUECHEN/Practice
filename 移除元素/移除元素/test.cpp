b807cef62fa24fcd09fc4b96597eb74dca06c9d4
YIUECHEN <2268691528@qq.com>
2020-01-16 21:44:28 +0800

    practice

---
+++ 移除元素/移除元素/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+
+}
+
+int main(){ 
+	vector<int> v = { 2, 3, 4, 5, 4, 4, 6, 6, 4 };
+	int k = 4;
+	cout<<removeElement(v, k)<<endl;
+
+	system("pause");
+	return 0;
+}
