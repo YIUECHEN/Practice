26ff61da0bd33e9ec966043e7325b705c5e27814
YIUECHEN <2268691528@qq.com>
2020-01-17 10:43:42 +0800

    change

--- 二叉树中第二小的节点/二叉树中第二小的节点/test.cpp
+++ 二叉树中第二小的节点/二叉树中第二小的节点/test.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-
 
 struct TreeNode {
 	int val;
@@ -9,36 +9,47 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-int findSecondMinimumValue(TreeNode* root) {
-	if (root->left == NULL&&root->right == NULL){
-		return -1;
-	}
+class Solution {
+public:
 
-	else {
-		if (root->left->val < root->right->val){
-			if (root->left->val < root->val){
-				return root->left->val;
+	int base;
+	int res;
+	int findSecondMinimumValue(TreeNode* root) {
+		base = root->val;
+		res = base;
+		fun(root);
+		return res == base ? -1 : res;
+	}
+	void fun(TreeNode* node){
+		if (node){
+			if (node->val > base){
+				if (res == base){
+					res = node->val;
+				}
+				else{
+					res = min(res, node->val);
+				}
+				return;
+
 			}
-			else{
-				if (root->right->val < root->val)
-				return root->right->val;
-			}
-		}
-		else{
-			return findSecondMinimumValue(root->left); 
+			fun(node->left);
+			fun(node->right);
 		}
 	}
-}
+};
+
+
 
 int main(){
-	TreeNode t1(2),t2(2),t3(3),t4(4),t5(5),t6(6),t7(7);
-	t1.left=&t2;
+	TreeNode t1(2), t2(2), t3(3), t4(4), t5(5), t6(6), t7(7);
+	t1.left = &t2;
 	t1.right = &t3;
 	t2.left = &t4;
 	t2.right = &t5;
 	t3.left = &t6;
 	t6.right = &t7;
-	cout << findSecondMinimumValue(&t1) << endl;
+	Solution s;
+	cout << s.findSecondMinimumValue(&t1) << endl;
 	system("pause");
 	return 0;
 }
