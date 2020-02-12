1c893cd7eee98005ba75196ee2b45a353a8e840d
YIUECHEN <2268691528@qq.com>
2020-02-12 10:32:42 +0800

    change

--- 子集/子集/test.cpp
+++ 子集/子集/test.cpp
@@ -1,24 +1,45 @@
 #include<iostream>
 #include<vector>
 using namespace  std;
-vector<vector<int>> subsets(vector<int>& nums) {
-	vector<vector<int>> v;
-	v.push_back({});
-	int size = nums.size();
-	int subsize = pow(2, size);
-	int tmp = 1;
-	while (tmp < subsize){
-		vector<int> temp;
-		for (int i = 0; i < size; i++){
-			int a = 1 << i;
-			if (a&tmp){
-				temp.push_back(nums[i]);
-			}
-		}
-		v.push_back(temp);
-		tmp++;
+//vector<vector<int>> subsets(vector<int>& nums) {
+//	vector<vector<int>> v;
+//	v.push_back({});
+//	int size = nums.size();
+//	int subsize = pow(2, size);
+//	int tmp = 1;
+//	while (tmp < subsize){
+//		vector<int> temp;
+//		for (int i = 0; i < size; i++){
+//			int a = 1 << i;
+//			if (a&tmp){
+//				temp.push_back(nums[i]);
+//			}
+//		}
+//		v.push_back(temp);
+//		tmp++;
+//	}
+//	return v;
+//}
+
+vector<vector<int>> ans;
+vector<int> tmp;
+void find(int n, vector<int> v){
+	//已经处理完最后一位，将目前存储的集合存入ans，并回溯
+	if (n <= 0){
+		ans.push_back(tmp);
+		return;
 	}
-	return v;
+	//情况一：集合中有该元素
+	tmp.push_back(v[n - 1]);
+	find(n - 1, v);
+	tmp.pop_back();
+	//情况二：集合中无该元素
+	find(n - 1, v);
+
+}
+vector<vector<int>> subsets(vector<int>& nums){
+	find(nums.size(), nums);
+	return ans;
 }
 
 int main(){
