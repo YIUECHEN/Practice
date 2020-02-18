a89fa3a52ffd2f48564fdab982a9c3b8733fd4cf
YIUECHEN <2268691528@qq.com>
2020-02-18 22:10:52 +0800

    change

--- 二叉树的前序遍历/二叉树的前序遍历/test.cpp
+++ 二叉树的前序遍历/二叉树的前序遍历/test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 struct TreeNode {
 	int val;
@@ -8,35 +9,68 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 	
 };
-vector<int> preorderTraversal(TreeNode* root) {
-	vector<int> ans;
-	TreeNode *node = root;
-	while (node){
-		if (node->left == NULL){
-			ans.emplace_back(node->val);
-			node = node->right;
-		}
-		else{
-			TreeNode *pre = node->left;
-			while (pre->right != NULL && pre->right != node){
-				pre = pre->right;
-			}
-			if (pre->right == NULL){
-				pre->right = node;
-				ans.emplace_back(node->val);
-				node = node->left;
-			}
-			else{
-				pre->right = NULL;
-				node = node->right;
-			}
-
-		}
-	}
-	return ans;
+//vector<int> preorderTraversal(TreeNode* root) {
+//	vector<int> ans;
+//	TreeNode *node = root;
+//	while (node){
+//		if (node->left == NULL){
+//			ans.emplace_back(node->val);
+//			node = node->right;
+//		}
+//		else{
+//			TreeNode *pre = node->left;
+//			while (pre->right != NULL && pre->right != node){
+//				pre = pre->right;
+//			}
+//			if (pre->right == NULL){
+//				pre->right = node;
+//				ans.emplace_back(node->val);
+//				node = node->left;
+//			}
+//			else{
+//				pre->right = NULL;
+//				node = node->right;
+//			}
+//
+//		}
+//	}
+//	return ans;
+//}
+#if 0
+void fun(TreeNode* root, vector<int> v){
+	if (root == NULL) return;
+	v.emplace_back(root->val);
+	fun(root->left);
+	fun(root->right);
 }
 
+vector<int> preorderTraversal(TreeNode* root) {
+	vector<int> v;
+	fun(root, v);
+	return v;
+}
 
 int main(){
+	return 0;
+}
+#endif
+
+vector<int> preorderTraversal(TreeNode* root) {
+	stack<TreeNode*> s;
+	vector<int> v;
+	while (root || s.size()){
+		while (root){
+			s.push(root);
+			v.emplace_back(root->val);
+			root = root->left;
+		}
+		root = s.top();
+		s.pop();
+		root = root->right;
+	}
+	return v;
+}
+int main(){
+
 	return 0;
 }
