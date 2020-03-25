152210cc6e5c047ddc5d4530b78d21a588286145
YIUECHEN <2268691528@qq.com>
2020-03-25 22:04:45 +0800

    practice

--- 美国节日/美国节日/test.cpp
+++ 美国节日/美国节日/test.cpp
@@ -9,7 +9,7 @@ int weektodays(int year, int month, int c, int w, bool b){
 	if (month == 2){
 		month = 14;
 		year--;
-	}
+	} 
 	i = 0;
 	for (d = b ? 1 : 31; d <= b ? 31 : 1; b ? (d++) : (d--)){
 		week = (d + 2 * month + 3 * (month + 1) / 5 + year + year / 4 - year / 100 + year / 400) % 7;
@@ -23,12 +23,12 @@ int main(){
 	int year;
 	while (cin >> year){
 		cout << year << "-01-01" << endl;
-		printf("%d-01-%d", year, weektodays(year, 1, 3, 1, 1));
-		printf("%d-02-%d", year, weektodays(year, 2, 3, 1, 1));
-		printf("%d-05-%d", year, weektodays(year, 5, 1, 1, 0));
+		printf("%d-01-%d\n", year, weektodays(year, 1, 3, 1, 1));
+		printf("%d-02-%d\n", year, weektodays(year, 2, 3, 1, 1));
+		printf("%d-05-%d\n", year, weektodays(year, 5, 1, 1, 0));
 		cout << year << "-07-04" << endl;
-		printf("%d-09-%d", year, weektodays(year, 9, 1, 1, 1));
-		printf("%d-11-%d", year, weektodays(year, 11, 4, 4, 1));
+		printf("%d-09-%d\n", year, weektodays(year, 9, 1, 1, 1));
+		printf("%d-11-%d\n", year, weektodays(year, 11, 4, 4, 1));
 		cout << year << "-012-25" << endl;
 	}
 	system("pause");
